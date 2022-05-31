cb7a5ecc07b5044ca23ab4ee64b2cf31732b9a49
110000110000 <98038223+110000110000@users.noreply.github.com>
2022-05-31 17:37:42 +0500

    Add files via upload

--- readMd.php
+++ readMd.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<script src='script.js'></script>
-  	<link rel="stylesheet" href="style.css">
+  	<link rel="stylesheet" href="mdstyle.css">
     <title>Document</title>
 </head>
 <body>
@@ -23,49 +23,6 @@ echo $Parsedown->text(file_get_contents($_GET['file']));
 
 
 
-<style>
-a{
-text-decoration:none
-}
-body {
-  background-color:white;
-  padding:50px 50px;
-}
-
-pre {
-  background-color:#eee;
-  overflow:auto;
-  margin:0 0 1em;
-  padding:.5em 1em;
-}
-
-pre code, pre .line-number {
-  font:normal normal 12px/14px "Courier New",Courier,Monospace;
-  color:black;
-  display:block;
-}
-
-pre .line-number {
-  float:left;
-  margin:0 1em 0 -1em;
-  border-right:1px solid;
-  text-align:right;
-}
-
-pre .line-number span {
-  display:block;
-  padding:0 .5em 0 1em;
-}
-
-pre .cl {
-  display:block;
-  clear:both;
-}
-blockquote{
-border-left: 5px solid #d0d7de;
-padding-left:10px;
-}
-</style>
 
 
 <script language="javascript">
